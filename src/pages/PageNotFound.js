ef07bf6ec889be1177e0e22c7c82b7c3d287fc28
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-10-14 19:10:46 +0300

    Not-found-page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,14 @@
 export const NotFoundPage = () => {
-    return <p>Page Not Foud!</p>;
-}
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh', // Make it full-height for vertical alignment
+        }}
+      >
+        <p style={{ fontSize: '2rem' }}>Page Not Found!</p>
+      </div>
+    );
+  }
